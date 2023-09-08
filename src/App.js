afd01de451f86846a6256ab2ea337c32cbb4ff51
Joe Christensen <joechristensenwork@gmail.com>
2023-09-08 11:00:04 -0700

    Setup (#2)

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import pianoCat from '../public/assets/piano_cat.gif'
+
+function App() {
+  return (
+    <div className="App">
+      <img src={pianoCat} alt="logo" />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+    </div>
+  );
+}
+
+export default App;
