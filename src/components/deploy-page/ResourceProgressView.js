fad36853eed2b570867267032dddfa5a6dc545c5
Joe Christensen <joechristensenwork@gmail.com>
2024-06-13 17:09:42 -0700

    Fixed hook

--- src/components/deploy-page/ResourceProgressView.js
+++ src/components/deploy-page/ResourceProgressView.js
@@ -1,7 +1,15 @@
 import { useState, useEffect } from 'react'
 import ResourceProgressViewer from 'components/ResourceProgressViewer/ResourceProgressViewer'
+import { formatResources } from 'components/ResourceProgressViewer/helpers'
 
 import { COMPLETED, FAILED, RUNNING } from 'constants/progress-statuses'
+
+const EVENT_STATUS_MAP = {
+  start: 'pending',
+  progress: 'running',
+  complete: 'completed',
+  errored: 'failed'
+}
 
 const ResourceProgressView = ({
   updateProvisioningStatus,
@@ -14,16 +22,17 @@ const ResourceProgressView = ({
     const containerLogsSocket = new WebSocket(`ws://127.0.0.1:8080/containers/logs?id=${containerId}`)
 
     containerLogsSocket.onmessage = event => {
-      // First event: change status to running
-      if (events.length === 0) {
+
+      setEvents(events => [...events, event.data])
+
+      const parsedEvent = JSON.parse(event.data)
+      const status = EVENT_STATUS_MAP[parsedEvent?.type?.split('_')?.[1]]
+      const data = parsedEvent?.hook || parsedEvent?.change
+
+      if (parsedEvent['@level'] !== undefined && status && ['create', 'update', 'delete'].includes(data?.action)) {
         updateProvisioningStatus({ status: RUNNING })
       }
 
-      // Add event to list
-      setEvents(events => [...events, event.data])
-
-      // Store artifact if there is one
-      const parsedEvent = JSON.parse(event.data)
       if (parsedEvent?.payload?.error_details?.match('artifact_created')) {
         const artifact = JSON.parse(JSON.parse(JSON.parse(parsedEvent.payload.error_details)?.detail)?.Message)?.payload?.artifact
 
