40742abb41045f227733954c5d5d782de13c0e42
Joe Christensen <joechristensenwork@gmail.com>
2025-06-25 11:06:21 -0700

    Disabled the local deploy flow in the devtool ui (#24)
    * Disabled the local deploy flow in the devtool ui
    
    * Removed back to app button
    
    * Removing commented code

--- src/components/layouts/JustHeaderLayout.js
+++ src/components/layouts/JustHeaderLayout.js
@@ -1,40 +1,24 @@
-/**
- * Must define the layout for any page requiring login. For now this only adds
- * the side bar, header bar, paper cups chat widget, and the context drawer.
- */
-import stylin from 'utils/stylin'
-import Header from 'components/Header'
+import Header from "components/Header"
 
-import Box from '@mui/material/Box'
-import Button from '@mui/material/Button'
-import ArrowRightIcon from '@mui/icons-material/ArrowRight'
-
+import Box from "@mui/material/Box"
 const JustHeaderLayout = ({ children }) => (
   <>
     <Box
-      width='100%'
+      width="100%"
       sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        height: '100vh',
-        overflow: 'hidden'
+        display: "flex",
+        flexDirection: "column",
+        height: "100vh",
+        overflow: "hidden",
       }}
     >
-      <Header>
-        <DeployButton
-          variant="outlined"
-          endIcon={<ArrowRightIcon sx={{ width: '25px', height: '25px' }} />}
-          href='/deploy'
-        >
-          Provision Bundle
-        </DeployButton>
-      </Header>
+      <Header></Header>
       <Box
         sx={{
-          width: '100%',
-          height: 'calc(100vh - 69px)',
-          overflowX: 'hidden',
-          overflowY: 'scroll'
+          width: "100%",
+          height: "calc(100vh - 69px)",
+          overflowX: "hidden",
+          overflowY: "scroll",
         }}
       >
         {children}
@@ -44,8 +28,3 @@ const JustHeaderLayout = ({ children }) => (
 )
 
 export default JustHeaderLayout
-
-const DeployButton = stylin(Button)({
-  borderRadius: '4px',
-  color: 'white'
-})
