00c4d88e5b6b899131dc644a27e0fcb78cb06482
Joe Christensen <joechristensenwork@gmail.com>
2025-06-25 10:49:27 -0700

    Removed back to app button

--- src/components/layouts/DeployHeaderLayout.js
+++ src/components/layouts/DeployHeaderLayout.js
@@ -2,39 +2,39 @@
  * Must define the layout for any page requiring login. For now this only adds
  * the side bar, header bar, paper cups chat widget, and the context drawer.
  */
-import stylin from 'utils/stylin'
-import Header from 'components/Header'
+// import stylin from 'utils/stylin'
+import Header from "components/Header"
 
-import Box from '@mui/material/Box'
-import Button from '@mui/material/Button'
-import ArrowLeftIcon from '@mui/icons-material/ArrowLeft'
+import Box from "@mui/material/Box"
+// import Button from '@mui/material/Button'
+// import ArrowLeftIcon from '@mui/icons-material/ArrowLeft'
 
 const DeployHeaderLayout = ({ children }) => (
   <>
     <Box
-      width='100%'
+      width="100%"
       sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        height: '100vh',
-        overflow: 'hidden'
+        display: "flex",
+        flexDirection: "column",
+        height: "100vh",
+        overflow: "hidden",
       }}
     >
       <Header>
-        <DeployButton
-          variant="outlined"
-          startIcon={<ArrowLeftIcon sx={{ width: '25px', height: '25px' }} />}
-          href='/'
-        >
-          Back to app
-        </DeployButton>
+        {/* <DeployButton
+        variant="outlined"
+        startIcon={<ArrowLeftIcon sx={{ width: '25px', height: '25px' }} />}
+        href='/'
+      >
+        Back to app
+      </DeployButton> */}
       </Header>
       <Box
         sx={{
-          width: '100%',
-          height: 'calc(100vh - 69px)',
-          overflowX: 'hidden',
-          overflowY: 'scroll'
+          width: "100%",
+          height: "calc(100vh - 69px)",
+          overflowX: "hidden",
+          overflowY: "scroll",
         }}
       >
         {children}
@@ -45,7 +45,7 @@ const DeployHeaderLayout = ({ children }) => (
 
 export default DeployHeaderLayout
 
-const DeployButton = stylin(Button)({
-  borderRadius: '4px',
-  color: 'white'
-})
+// const DeployButton = stylin(Button)({
+//   borderRadius: '4px',
+//   color: 'white'
+// })
