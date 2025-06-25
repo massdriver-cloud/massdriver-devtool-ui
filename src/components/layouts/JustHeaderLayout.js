57d9258ab078e558175bc014b62b2d9e3a3c5efd
Joe Christensen <joechristensenwork@gmail.com>
2025-06-25 10:46:34 -0700

    Disabled the local deploy flow in the devtool ui

--- src/components/layouts/JustHeaderLayout.js
+++ src/components/layouts/JustHeaderLayout.js
@@ -2,39 +2,39 @@
  * Must define the layout for any page requiring login. For now this only adds
  * the side bar, header bar, paper cups chat widget, and the context drawer.
  */
-import stylin from 'utils/stylin'
-import Header from 'components/Header'
+import stylin from "utils/stylin"
+import Header from "components/Header"
 
-import Box from '@mui/material/Box'
-import Button from '@mui/material/Button'
-import ArrowRightIcon from '@mui/icons-material/ArrowRight'
+import Box from "@mui/material/Box"
+// import Button from "@mui/material/Button"
+// import ArrowRightIcon from "@mui/icons-material/ArrowRight"
 
 const JustHeaderLayout = ({ children }) => (
   <>
     <Box
-      width='100%'
+      width="100%"
       sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        height: '100vh',
-        overflow: 'hidden'
+        display: "flex",
+        flexDirection: "column",
+        height: "100vh",
+        overflow: "hidden",
       }}
     >
       <Header>
-        <DeployButton
+        {/* <DeployButton
           variant="outlined"
           endIcon={<ArrowRightIcon sx={{ width: '25px', height: '25px' }} />}
           href='/deploy'
         >
           Provision Bundle
-        </DeployButton>
+        </DeployButton> */}
       </Header>
       <Box
         sx={{
-          width: '100%',
-          height: 'calc(100vh - 69px)',
-          overflowX: 'hidden',
-          overflowY: 'scroll'
+          width: "100%",
+          height: "calc(100vh - 69px)",
+          overflowX: "hidden",
+          overflowY: "scroll",
         }}
       >
         {children}
@@ -45,7 +45,7 @@ const JustHeaderLayout = ({ children }) => (
 
 export default JustHeaderLayout
 
-const DeployButton = stylin(Button)({
-  borderRadius: '4px',
-  color: 'white'
-})
+// const DeployButton = stylin(Button)({
+//   borderRadius: "4px",
+//   color: "white",
+// })
--- src/pages/DeployPage.js
+++ src/pages/DeployPage.js
@@ -1,64 +1,71 @@
-import { useState } from 'react'
-import stylin from 'utils/stylin'
+// import { useState } from 'react'
+// import stylin from 'utils/stylin'
 
-import useGetDeploySteps from 'hooks/queries/useGetDeploySteps'
-import Stepper from 'components/Stepper/Stepper.container'
-import LoadingSpinner from 'components/LoadingSpinner'
-import ProvisionInfo from 'components/deploy-page/ProvisionInfo.container'
-import { INITIALIZED } from 'constants/progress-statuses'
+// import useGetDeploySteps from 'hooks/queries/useGetDeploySteps'
+// import Stepper from 'components/Stepper/Stepper.container'
+// import LoadingSpinner from 'components/LoadingSpinner'
+// import ProvisionInfo from 'components/deploy-page/ProvisionInfo.container'
+// import { INITIALIZED } from 'constants/progress-statuses'
 
-import Typography from '@mui/material/Typography'
-
+import Typography from "@mui/material/Typography"
+import Custom404 from "components/Custom404"
 const DeployPage = () => {
-  const { data, loading, error } = useGetDeploySteps()
-
-  const [provisioningStatus, setProvisioningStatus] = useState(INITIALIZED)
-  const [provisioningAction, setProvisioningAction] = useState('')
-  const [containerId, setContainerId] = useState('')
-  const [artifacts, setArtifacts] = useState([])
-  const [deploymentEvents, setDeploymentEvents] = useState([])
-
-  const updateProvisioningStatus = ({ status, action, containerId, artifact, deploymentEvents }) => {
-    if (containerId) {
-      setContainerId(containerId)
-      setProvisioningStatus(INITIALIZED)
-      setProvisioningAction('')
-      setArtifacts([])
-      setDeploymentEvents([])
-    }
-    status && setProvisioningStatus(status)
-    action && setProvisioningAction(action)
-    artifact && setArtifacts(artifacts => [...artifacts, artifact])
-    deploymentEvents && setDeploymentEvents(deploymentEvents)
-  }
-
-  return loading ? (
-    <LoadingSpinner />
-  ) : error ? (
-    <EmptyMessage>Error: There was an issue parsing your configuration form.</EmptyMessage>
-  ) : provisioningStatus === INITIALIZED ? (
-    <Stepper
-      steps={data}
-      childProps={{ updateProvisioningStatus }}
-    />
-  ) : (
-    <ProvisionInfo
-      status={provisioningStatus}
-      action={provisioningAction}
-      updateProvisioningStatus={updateProvisioningStatus}
-      containerId={containerId}
-      artifacts={artifacts}
-      deploymentEvents={deploymentEvents}
-    />
+  return (
+    <Custom404>
+      <Typography sx={{ fontSize: "4rem", fontWeight: "bold" }}>404</Typography>
+      <Typography variant="h1">Page not found</Typography>
+    </Custom404>
   )
+
+  // const { data, loading, error } = useGetDeploySteps()
+
+  // const [provisioningStatus, setProvisioningStatus] = useState(INITIALIZED)
+  // const [provisioningAction, setProvisioningAction] = useState('')
+  // const [containerId, setContainerId] = useState('')
+  // const [artifacts, setArtifacts] = useState([])
+  // const [deploymentEvents, setDeploymentEvents] = useState([])
+
+  // const updateProvisioningStatus = ({ status, action, containerId, artifact, deploymentEvents }) => {
+  //   if (containerId) {
+  //     setContainerId(containerId)
+  //     setProvisioningStatus(INITIALIZED)
+  //     setProvisioningAction('')
+  //     setArtifacts([])
+  //     setDeploymentEvents([])
+  //   }
+  //   status && setProvisioningStatus(status)
+  //   action && setProvisioningAction(action)
+  //   artifact && setArtifacts(artifacts => [...artifacts, artifact])
+  //   deploymentEvents && setDeploymentEvents(deploymentEvents)
+  // }
+
+  // return loading ? (
+  //   <LoadingSpinner />
+  // ) : error ? (
+  //   <EmptyMessage>Error: There was an issue parsing your configuration form.</EmptyMessage>
+  // ) : provisioningStatus === INITIALIZED ? (
+  //   <Stepper
+  //     steps={data}
+  //     childProps={{ updateProvisioningStatus }}
+  //   />
+  // ) : (
+  //   <ProvisionInfo
+  //     status={provisioningStatus}
+  //     action={provisioningAction}
+  //     updateProvisioningStatus={updateProvisioningStatus}
+  //     containerId={containerId}
+  //     artifacts={artifacts}
+  //     deploymentEvents={deploymentEvents}
+  //   />
+  // )
 }
 
 export default DeployPage
 
-const EmptyMessage = stylin(Typography)(({ theme }) => ({
-  fontSize: '14px',
-  color: theme.palette.grey[900],
-  fontWeight: 'bold',
-  px: '30px',
-  py: '20px'
-}))
+// const EmptyMessage = stylin(Typography)(({ theme }) => ({
+//   fontSize: '14px',
+//   color: theme.palette.grey[900],
+//   fontWeight: 'bold',
+//   px: '30px',
+//   py: '20px'
+// }))
